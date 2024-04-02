5bf74a31d53fdc27aec47449ba1ae1b6167242b3
fabien-ricca <fabien.ricca@laplateforme.io>
2024-04-02 12:03:31 +0200

    [ Jour 01 ] -- { Job 02 }

---
+++ Jour_01/Job-02/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../headers/main.hpp"
+
+
+int main()
+{
+    int nb1 = 3;
+    int nb2 = 4;
+    int result = nb1 * nb2;
+
+   std::cout << nb1 << " x " << nb2 << " = " << result << std::endl;
+
+    return 0;
+}
